68c0d4886cbca506f10aa8bc2cc12a086db77415
Roger Flores Vieira <rogerfloresvieira@icloud.com>
2022-08-07 19:29:13 -0400

    Time: 56 ms (29.24%), Space: 13.9 MB (11.16%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,5 +1,5 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        return sum(i in jewels for i in stones)
+        return sum(item in jewels for item in stones)
         
                 
