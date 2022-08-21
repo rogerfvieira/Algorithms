c5c163ea8bea0497d8c56147ae3fe56ec15f0758
Roger Flores Vieira <rogerfloresvieira@icloud.com>
2022-08-21 15:12:09 -0400

    Delete 1265-print-immutable-linked-list-in-reverse directory

--- 1265-print-immutable-linked-list-in-reverse/1265-print-immutable-linked-list-in-reverse.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def printLinkedListInReverse(self, head: 'ImmutableListNode') -> None:
-  
-         if head:
-            self.printLinkedListInReverse(head.getNext())
-            head.printValue()
