c35126d029dbf410417c23b029280c91c40ec096
Roger Flores Vieira <rogerfloresvieira@icloud.com>
2022-08-21 15:10:21 -0400

    Time: 71 ms (21.36%), Space: 15 MB (33.88%) - LeetHub

--- 1265-print-immutable-linked-list-in-reverse/1265-print-immutable-linked-list-in-reverse.py
+++ 1265-print-immutable-linked-list-in-reverse/1265-print-immutable-linked-list-in-reverse.py
@@ -1,6 +1,6 @@
 class Solution:
     def printLinkedListInReverse(self, head: 'ImmutableListNode') -> None:
-        
+  
          if head:
-            self.printLinkedListInReverse(head.getNext());
-            head.printValue();
+            self.printLinkedListInReverse(head.getNext())
+            head.printValue()
