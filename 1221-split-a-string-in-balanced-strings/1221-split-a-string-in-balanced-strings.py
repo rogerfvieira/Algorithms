73edcfd652e18f7ce5dd3221b2c4ec040a2c75d3
Roger Flores Vieira <rogerfloresvieira@icloud.com>
2022-08-23 16:01:34 -0400

    Time: 53 ms (39.30%), Space: 13.9 MB (57.79%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
@@ -2,7 +2,7 @@ class Solution:
     def balancedStringSplit(self, s: str) -> int:
         counter_R=0
         counter_L=0
-        tracker=[]
+        counter=0
         
         for index,element in enumerate(s):
             if element == 'R':
@@ -10,14 +10,11 @@ class Solution:
                 if counter_R == counter_L:
                     counter_L = 0
                     counter_R = 0
-                    tracker.append(s[:index+1])
-                    s=s[index+1:]
+                    counter+=1
             else:
                 counter_L+=1
                 if counter_R == counter_L:
                     counter_L = 0
                     counter_R = 0
-                    tracker.append(s[:index+1])
-                    s=s[index+1:]
-        
-        return len(tracker)
+                    counter+=1
+        return counter
