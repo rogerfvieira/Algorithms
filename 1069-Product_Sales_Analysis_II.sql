ae2cd2e9459a6c697ec12b5fdb64dcd1457ab321
Roger Flores Vieira <rogerfloresvieira@icloud.com>
2022-11-26 16:50:34 -0500

    Create 1069-Product_Sales_Analysis_II.sql

---
+++ 1069-Product_Sales_Analysis_II.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+SELECT Sales.product_id, SUM(quantity) AS total_quantity
+FROM sales
+INNER JOIN Product
+ON Sales.product_id = Product.product_id
+GROUP BY Sales.product_id;
