92aaef553ad466116701045692b0f9a4bf95830d
Roger Flores Vieira <rogerfloresvieira@icloud.com>
2022-11-27 15:08:15 -0500

    Create 2329-Product_Sales_Analysis_V.sql

---
+++ 2329-Product_Sales_Analysis_V.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT Sales.user_id, SUM(quantity * Product.price) AS spending
+FROM sales
+INNER JOIN Product
+ON Sales.product_id = Product.product_id
+GROUP BY 1
+ORDER BY 2 DESC, 1  ASC;
