36176eb3063edd92dc732b4a3cfc6d62e7e2da3c
rogerfvieira <rogerfloresvieira@icloud.com>
2022-11-28 20:54:52 -0500

    clean up

--- 1051-height-checker/1051-height-checker.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
-        height_b=sorted(heights)
-        return sum([1 for index,element in enumerate(heights) if heights[index]!=height_b[index]])
