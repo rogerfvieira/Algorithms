72bb762b81ff3d2adf28a242d6c6a82f46a1ca96
rogerfvieira <rogerfloresvieira@icloud.com>
2022-12-01 14:49:19 -0500

    problem 1571

---
+++ sql/1571-Warehouse-Manager.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT name AS warehouse_name, SUM(Warehouse.units* Products.Width * Products.Length * Products.Height) AS volume
+FROM Warehouse
+INNER JOIN Products ON Warehouse.product_id = Products.product_id
+GROUP BY name;
