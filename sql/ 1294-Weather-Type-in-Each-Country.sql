d331dc0d7cf8bbb4fcb4057d7fd288ef40c66b7a
rogerfvieira <rogerfloresvieira@icloud.com>
2022-12-03 16:58:20 -0500

    problem 1294

---
+++ sql/ 1294-Weather-Type-in-Each-Country.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+SELECT country_name,
+       CASE 
+         WHEN AVG(weather.weather_state) <=15 THEN 'Cold'
+         WHEN AVG(weather.weather_state) >=25 THEN 'Hot'
+         ELSE 'Warm'
+         END AS weather_type
+FROM Countries
+INNER JOIN Weather ON Countries.country_id = Weather.country_id
+WHERE DATE_FORMAT(day,'%M %Y') = 'November 2019'
+GROUP BY 1;
