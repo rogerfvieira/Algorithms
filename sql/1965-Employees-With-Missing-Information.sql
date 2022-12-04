46cc9230fab97fce2a806fe43310c34811303bdb
rogerfvieira <rogerfloresvieira@icloud.com>
2022-12-04 12:41:40 -0500

    problem 2377

--- sql/1965-Employees-With-Missing-Information.sql
+++ sql/1965-Employees-With-Missing-Information.sql
@@ -1,4 +1,4 @@
-Write your MySQL query statement below
+#Write your MySQL query statement below
 
 SELECT employee_id
 FROM Employees
---
+++ sql/2377-Sort-the-Olympic-Table.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+
+SELECT *
+FROM Olympic
+ORDER BY gold_medals DESC, silver_medals DESC, bronze_medals DESC, country ASC
