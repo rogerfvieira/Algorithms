e13bb775c79067ef392ac89648cce22a87a3e348
rogerfvieira <rogerfloresvieira@icloud.com>
2022-12-04 12:53:01 -0500

    problem 2082

---
+++ sql/2082-The-Number-of-Rich-Customers.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT COUNT(DISTINCT(customer_id)) AS rich_count
+FROM Store
+WHERE store.customer_id IN (SELECT DISTINCT(customer_id)
+FROM Store
+WHERE amount > 500
+GROUP BY bill_id)
