3d15dbdb63ee652801411b343fc593b8ec5cbef7
rogerfvieira <rogerfloresvieira@icloud.com>
2022-12-10 14:14:43 -0500

    problem 1076

---
+++ sql/1076-Project-Employees-II.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+WITH table_count AS (SELECT project_id,COUNT(*) as total
+FROM Project
+INNER JOIN Employee ON Project.employee_id = Employee.employee_id
+GROUP BY project_id
+ORDER BY COUNT(*) DESC)
+
+SELECT project_id
+FROM Project
+INNER JOIN Employee ON Project.employee_id = Employee.employee_id
+GROUP BY project_id
+HAVING COUNT(project_id) = (SELECT MAX(total) FROM table_count)
