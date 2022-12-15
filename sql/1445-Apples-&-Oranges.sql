5d796cb63c08e243eeb973bdf7bbec2206f83b60
rogerfvieira <rogerfloresvieira@icloud.com>
2022-12-15 13:45:40 -0500

    problem 1445

---
+++ sql/1445-Apples-&-Oranges.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+WITH apples AS(
+SELECT sale_date, sold_num AS 'sales'
+FROM Sales
+WHERE fruit = 'apples'),
+
+oranges AS (
+SELECT sale_date, sold_num AS 'sales'
+FROM Sales
+WHERE fruit = 'oranges'
+)
+
+SELECT apples.sale_date, apples.sales-oranges.sales AS diff
+FROM apples
+INNER JOIN oranges ON apples.sale_date = oranges.sale_date
